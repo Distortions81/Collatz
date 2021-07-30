3b7ad2d8d28518d452cb6745e07f0b36f6fe1c78
Distortions81 <33408758+Distortions81@users.noreply.github.com>
2021-07-30 14:26:50 -0600

    show cpu that found

--- main.go
+++ main.go
@@ -11,29 +11,29 @@ var MaxLock sync.Mutex
 
 const maxInt = 18446744073709551615 // 2^64-1
 
-func checkMaxSteps(seed uint64, i uint64, steps uint64) {
+func checkMaxSteps(cpu int, seed uint64, i uint64, steps uint64) {
 	MaxLock.Lock()
 	if steps > MaxSteps {
 		MaxSteps = steps
-		fmt.Printf("NEW RECORD LENGTH:\nSEED: %v, I: %v, STEPS: %v\n", seed, i, steps)
+		fmt.Printf("RECORD (CPU %v): SEED: %v, I: %v, STEPS: %v\n", cpu, seed, i, steps)
 	}
 	MaxLock.Unlock()
 }
 
-func collatz(seed uint64, i uint64, steps uint64) {
+func collatz(cpu int, seed uint64, i uint64, steps uint64) {
 
 	if steps%1000000 == 0 {
-		checkMaxSteps(seed, i, steps)
+		checkMaxSteps(cpu, seed, i, steps)
 	}
 
 	if i <= 1 {
-		checkMaxSteps(seed, i, steps)
+		checkMaxSteps(cpu, seed, i, steps)
 	} else if i%2 == 0 {
 		i = i / 2
-		collatz(seed, i, steps+1)
+		collatz(cpu, seed, i, steps+1)
 	} else {
 		i = i*3 + 1
-		collatz(seed, i, steps+1)
+		collatz(cpu, seed, i, steps+1)
 	}
 }
 
@@ -50,7 +50,7 @@ func main() {
 		fmt.Printf("CPU: %v, Work area: %v to %v\n", cpu, workStart, workEnd)
 		go func(workStart uint64, workEnd uint64, cpu int) {
 			for x := workStart; x < workEnd; x++ {
-				collatz(x, x, 0)
+				collatz(cpu, x, x, 0)
 			}
 			fmt.Printf("\nvCPU %v IS FINISHED\n", cpu)
 		}(workStart, workEnd, cpu)
